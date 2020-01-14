b5e76a7e96174bfffbfa8b1aeda9cc2564ccdf3a
Amit4ate <amit.itaas@gmail.com>
2020-01-14 08:08:09 +0530

    Added Initial code for Presto Functional Testing

--- src/test/java/com/presto/testPrestoHomePage.java
+++ src/test/java/com/presto/testPrestoHomePage.java
@@ -4,16 +4,19 @@ import org.openqa.selenium.WebDriver;
         import org.openqa.selenium.firefox.FirefoxDriver;
         import org.openqa.selenium.ie.InternetExplorerDriver;
         import org.openqa.selenium.remote.DesiredCapabilities;
-        import org.testng.annotations.Parameters;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
         import org.testng.annotations.Test;
 
-public class PrestoHomePage
-{   @Test
-    public void PrestoHomePage()
-    {
+public class testPrestoHomePage
+{
+    @Test
+    @Parameters ({ "appURL" })
+     public void PrestoHomePage(String appURL) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\amit5india\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://almsmart.demos.hclets.com/Presto/");
+        driver.get(appURL);
+        driver
 
 }
 
