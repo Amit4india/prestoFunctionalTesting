1317e35c4078ac4d53e39cf4eccfa1203e562314
Amit4ate <amit.itaas@gmail.com>
2020-01-14 17:10:30 +0530

    Modified testng.xml

--- src/test/java/com/presto/testPrestoHomePage.java
+++ src/test/java/com/presto/testPrestoHomePage.java
@@ -1,21 +1,28 @@
 package com.presto;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
         import org.openqa.selenium.chrome.ChromeDriver;
         import org.openqa.selenium.firefox.FirefoxDriver;
         import org.openqa.selenium.ie.InternetExplorerDriver;
         import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.BeforeClass;
         import org.testng.annotations.Test;
 
-public class testPrestoHomePage
-{
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class testPrestoHomePage {
     @Test
-       public void PrestoHomePage() throws InterruptedException {
-       // System.setProperty("webdriver.chrome.driver", "C:\\Users\\amit5india\\Downloads\\chromedriver_win32\\chromedriver.exe");
+    public void PrestoHomePage() throws InterruptedException {
+        // System.setProperty("webdriver.chrome.driver", "C:\\Users\\amit5india\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://almsmart.demos.hclets.com/Presto/");
+        System.out.println("This is test 1");
 
+    }
 }
 
-}
